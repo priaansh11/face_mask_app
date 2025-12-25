7f2ecf38cb013356b872d30aad1df8c30d0ac97b
Priyanshu Kumar <145970530+priaansh11@users.noreply.github.com>
2025-12-25 18:04:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if uploaded_file is not None:
     col1, col2 = st.columns(2)
     
     with col1:
-        st.image(img_resized, caption="Uploaded Image", use_container_width=True)
+        st.image(img_resized, caption="Uploaded Image", use_column_width=True)
     
     with col2:
         st.subheader("Prediction Result")
